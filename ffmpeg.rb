c7d2f542752109aa42663b0be31eca8a1990917c
David Christenson <mail@noctem.xyz>
2019-02-04 20:27:04 -0700

    Add ffmpeg

---
+++ ffmpeg.rb
@@ -0,0 +1,91 @@
+class Ffmpeg < Formula
+  desc "Play, record, convert, and stream audio and video"
+  homepage "https://ffmpeg.org/"
+  url "https://ffmpeg.org/releases/ffmpeg-4.1.tar.xz"
+  sha256 "a38ec4d026efb58506a99ad5cd23d5a9793b4bf415f2c4c2e9c1bb444acd1994"
+  revision 3
+  head "https://github.com/FFmpeg/FFmpeg.git"
+
+  bottle do
+    rebuild 2
+    sha256 "2dbbbc057c3d447510bf739e6822d03765b3d55669f74e96dd7258cc1976cbf1" => :mojave
+    sha256 "440bf019cdf9739d3d13bd32b0a96d15a34a50ae7ead31b41180ff66eee1b316" => :high_sierra
+    sha256 "261e0b798358f83f14f1ab7168f8ce1e50a8da28307485315aea0e4a94235197" => :sierra
+  end
+
+  depends_on "nasm" => :build
+  depends_on "pkg-config" => :build
+  depends_on "texi2html" => :build
+
+  depends_on "aom"
+  depends_on "fontconfig"
+  depends_on "freetype"
+  depends_on "frei0r"
+  depends_on "lame"
+  depends_on "libass"
+  depends_on "libvorbis"
+  depends_on "libvpx"
+  depends_on "opencore-amr"
+  depends_on "opus"
+  depends_on "rtmpdump"
+  depends_on "sdl2"
+  depends_on "snappy"
+  depends_on "speex"
+  depends_on "theora"
+  depends_on "x264"
+  depends_on "x265"
+  depends_on "xvid"
+  depends_on "xz"
+
+  def install
+    args = %W[
+      --prefix=#{prefix}
+      --enable-shared
+      --enable-pthreads
+      --enable-version3
+      --enable-hardcoded-tables
+      --enable-avresample
+      --cc=#{ENV.cc}
+      --host-cflags=#{ENV.cflags}
+      --host-ldflags=#{ENV.ldflags}
+      --enable-ffplay
+      --enable-gpl
+      --enable-libaom
+      --enable-libmp3lame
+      --enable-libopus
+      --enable-libsnappy
+      --enable-libtheora
+      --enable-libvorbis
+      --enable-libvpx
+      --enable-libx264
+      --enable-libx265
+      --enable-libxvid
+      --enable-lzma
+      --enable-libfontconfig
+      --enable-libfreetype
+      --enable-frei0r
+      --enable-libass
+      --enable-libopencore-amrnb
+      --enable-libopencore-amrwb
+      --enable-librtmp
+      --enable-libspeex
+      --enable-videotoolbox
+      --disable-libjack
+      --disable-indev=jack
+    ]
+
+    system "./configure", *args
+    system "make", "install"
+
+    # Build and install additional FFmpeg tools
+    system "make", "alltools"
+    bin.install Dir["tools/*"].select { |f| File.executable? f }
+  end
+
+  test do
+    # Create an example mp4 file
+    mp4out = testpath/"video.mp4"
+    system bin/"ffmpeg", "-filter_complex", "testsrc=rate=1:duration=1", mp4out
+    assert_predicate mp4out, :exist?
+  end
+end
