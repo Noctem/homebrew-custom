65213f6820c51eb7b201841a68d8045d20b90888
David Christenson <mail@noctem.xyz>
2019-02-24 22:08:09 -0700

    Add VapourSynth
    Add VapourSynth with ImageMagick (imwri) support and use brewed Cython.

---
+++ vapoursynth.rb
@@ -0,0 +1,37 @@
+class Vapoursynth < Formula
+  include Language::Python::Virtualenv
+
+  desc "Video processing framework with simplicity in mind"
+  homepage "http://www.vapoursynth.com"
+  url "https://github.com/vapoursynth/vapoursynth/archive/R45.1.tar.gz"
+  sha256 "4f43e5bb8c4817fdebe572d82febe4abac892918c54e1cb71aa6f6eb3677a877"
+  head "https://github.com/vapoursynth/vapoursynth.git"
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "cython" => :build
+  depends_on "libtool" => :build
+  depends_on "nasm" => :build
+  depends_on "pkg-config" => :build
+
+  depends_on "imagemagick"
+  depends_on "libass"
+  depends_on :macos => :el_capitan # due to zimg dependency
+  depends_on "python"
+  depends_on "tesseract"
+  depends_on "zimg"
+
+  def install
+    system "./autogen.sh"
+    system "./configure", "--prefix=#{prefix}",
+                          "--with-cython=#{Formula["cython"].opt_bin}/cython"
+    system "make", "install"
+  end
+
+  test do
+    py3 = Language::Python.major_minor_version "python3"
+    ENV.prepend_path "PYTHONPATH", lib/"python#{py3}/site-packages"
+    system "python3", "-c", "import vapoursynth"
+    system bin/"vspipe", "--version"
+  end
+end
