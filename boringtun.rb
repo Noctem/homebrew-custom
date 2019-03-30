95064116a87cc7a0dab1fa70e1941af7e6539fcb
David Christenson <mail@noctem.xyz>
2019-03-29 18:28:49 -0600

    boringtun: add proper release tarball

--- boringtun.rb
+++ boringtun.rb
@@ -1,9 +1,8 @@
 class Boringtun < Formula
   desc "Userspace WireGuard implementation in Rust"
   homepage "https://github.com/cloudflare/boringtun"
-  url "https://github.com/cloudflare/boringtun/archive/vTEST2.tar.gz"
-  sha256 "011fd5a8230ca7314d93cdc75479380453fa4882a56778ae1cc52d23f312b3ad"
-  version "0.2.0"
+  url "https://github.com/cloudflare/boringtun/archive/v0.2.0.tar.gz"
+  sha256 "544c72fc482b636e7f6460bfee205adafc55de534067819e4e4914980f0d1350"
   head "https://github.com/cloudflare/boringtun.git"
 
   depends_on "rust" => :build
