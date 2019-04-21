d70a1f7b5a13f82404be455f2e5442f9a83ac03a
David Christenson <mail@noctem.xyz>
2019-04-21 16:40:16 -0600

    libtorrent-rasterbar@1.1 1.1.2 (new formula)

---
+++ libtorrent-rasterbar@1.1.rb
@@ -0,0 +1,55 @@
+class LibtorrentRasterbarAT11 < Formula
+  desc "C++ bittorrent library by Rasterbar Software"
+  homepage "https://www.libtorrent.org/"
+  url "https://github.com/arvidn/libtorrent/releases/download/libtorrent_1_1_12/libtorrent-rasterbar-1.1.12.tar.gz"
+  sha256 "a5937134edf3ca8c109403a47f5a5fc93f7b8dca4e97f1a629a8c84288bee7a7"
+
+  head do
+    url "https://github.com/arvidn/libtorrent.git", :branch => "RC_1_1"
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+  end
+
+  keg_only :versioned_formula
+
+  depends_on "pkg-config" => :build
+  depends_on "boost"
+  depends_on "boost-python3"
+  depends_on "openssl"
+  depends_on "python"
+
+  def install
+    args = %W[
+      --disable-debug
+      --disable-dependency-tracking
+      --disable-silent-rules
+      --prefix=#{prefix}
+      --enable-encryption
+      --enable-python-binding
+      --with-boost=#{Formula["boost"].opt_prefix}
+      --with-boost-python=boost_python37-mt
+      PYTHON=python3
+    ]
+
+    if build.head?
+      system "./bootstrap.sh", *args
+    else
+      system "./configure", *args
+    end
+
+    system "make", "install"
+    libexec.install "examples"
+  end
+
+  test do
+    system ENV.cxx, "-std=c++11", "-I#{Formula["boost"].include}/boost",
+                    "-L#{lib}", "-ltorrent-rasterbar",
+                    "-L#{Formula["boost"].lib}", "-lboost_system",
+                    "-framework", "SystemConfiguration",
+                    "-framework", "CoreFoundation",
+                    libexec/"examples/make_torrent.cpp", "-o", "test"
+    system "./test", test_fixtures("test.mp3"), "-o", "test.torrent"
+    assert_predicate testpath/"test.torrent", :exist?
+  end
+end
