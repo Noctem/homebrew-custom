77fd460179928036503a3682c54287b819b4be9b
David Christenson <mail@noctem.xyz>
2019-09-27 05:36:44 -0600

    Rust 1.38
    Also use brewed LLVM since that is working now.

--- rust.rb
+++ rust.rb
@@ -1,29 +1,29 @@
 class Rust < Formula
   desc "Safe, concurrent, practical language"
   homepage "https://www.rust-lang.org/"
-  url "https://static.rust-lang.org/dist/rustc-1.37.0-src.tar.gz"
-  sha256 "120e7020d065499cc6b28759ff04153bfdc2ac9b5adeb252331a4eb87cbe38c3"
+  url "https://static.rust-lang.org/dist/rustc-1.38.0-src.tar.gz"
+  sha256 "644263ca7c7106f8ee8fcde6bb16910d246b30668a74be20b8c7e0e9f4a52d80"
   head "https://github.com/rust-lang/rust.git"
-  revision 1
 
   depends_on "cmake" => :build
+  depends_on "llvm" => :build
   depends_on "python@2" => :build
   depends_on "libssh2"
   depends_on "openssl@1.1"
   depends_on "pkg-config"
 
   def install
-    copy_file "config.toml.example", "config.toml"
+    mv "config.toml.example", "config.toml"
     inreplace "config.toml" do |s|
       s.gsub! '#prefix = "/usr/local"', "prefix = \"#{prefix}\""
       s.gsub! '#sysconfdir = "/etc"', "sysconfdir = \"#{etc}\""
       s.gsub! "#extended = false", "extended = true"
       s.gsub! '#channel = "dev"', 'channel = "stable"'
       s.gsub! '[target.x86_64-unknown-linux-gnu]', '[target.x86_64-apple-darwin]'
-      #s.gsub! '#cc = "cc"', 'cc = "/usr/local/opt/llvm/bin/clang"'
+      s.gsub! '#cc = "cc"', 'cc = "/usr/local/opt/llvm/bin/clang"'
       s.gsub! '#ar = "ar"', 'ar = "/usr/local/opt/llvm/bin/llvm-ar"'
       s.gsub! '#ranlib = "ranlib"', 'ranlib = "/usr/local/opt/llvm/bin/llvm-ranlib"'
-      #s.gsub! '#llvm-config = "../path/to/llvm/root/bin/llvm-config"', 'llvm-config = "/usr/local/opt/llvm/bin/llvm-config"'
+      s.gsub! '#llvm-config = "../path/to/llvm/root/bin/llvm-config"', 'llvm-config = "/usr/local/opt/llvm/bin/llvm-config"'
     end
 
     # Fix build failure for compiler_builtins "error: invalid deployment target
@@ -33,8 +33,8 @@ class Rust < Formula
     # Ensure that the `openssl` crate picks up the intended library.
     # https://crates.io/crates/openssl#manual-configuration
     ENV["OPENSSL_DIR"] = Formula["openssl@1.1"].opt_prefix
-    #ENV["CC"] = "/usr/local/opt/llvm/bin/clang"
-    #ENV["CXX"] = "/usr/local/opt/llvm/bin/clang++"
+    ENV["CC"] = "/usr/local/opt/llvm/bin/clang"
+    ENV["CXX"] = "/usr/local/opt/llvm/bin/clang++"
 
     # Fix build failure for cmake v0.1.24 "error: internal compiler error:
     # src/librustc/ty/subst.rs:127: impossible case reached" on 10.11, and for
