cfe284df8582a892b3c17cbb917b2a70ec0efb66
David Christenson <mail@noctem.xyz>
2019-10-27 21:04:39 -0600

    mpv 0.30.0

--- mpv.rb
+++ mpv.rb
@@ -1,19 +1,20 @@
 class Mpv < Formula
   desc "Media player based on MPlayer and mplayer2"
   homepage "https://mpv.io"
+  url "https://github.com/mpv-player/mpv/archive/v0.30.0.tar.gz"
+  sha256 "33a1bcb7e74ff17f070e754c15c52228cf44f2cefbfd8f34886ae81df214ca35"
   head "https://github.com/mpv-player/mpv.git"
 
   depends_on "docutils" => :build
   depends_on "pkg-config" => :build
   depends_on "python" => :build
-
   depends_on "vapoursynth" => :recommended
   depends_on "noctem/custom/ffmpeg"
+  depends_on "noctem/custom/lua@5.2"
   depends_on "jpeg"
   depends_on "libarchive"
   depends_on "libass"
   depends_on "little-cms2"
-  depends_on "lua@5.1"
   depends_on "mujs"
   depends_on "uchardet"
   depends_on "youtube-dl"
