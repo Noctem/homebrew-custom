2353a386f7e169470c8affdef03965264dae9d8c
David Christenson <mail@noctem.xyz>
2019-11-07 18:46:26 -0700

    Rust 1.39.0

--- rust.rb
+++ rust.rb
@@ -1,12 +1,11 @@
 class Rust < Formula
   desc "Safe, concurrent, practical language"
   homepage "https://www.rust-lang.org/"
-  url "https://static.rust-lang.org/dist/rustc-1.38.0-src.tar.gz"
-  sha256 "644263ca7c7106f8ee8fcde6bb16910d246b30668a74be20b8c7e0e9f4a52d80"
+  url "https://static.rust-lang.org/dist/rustc-1.39.0-src.tar.gz"
+  sha256 "b4a1f6b6a93931f270691aba4fc85eee032fecda973e6b9c774cd06857609357"
   head "https://github.com/rust-lang/rust.git"
 
   depends_on "cmake" => :build
-  depends_on "llvm" => :build
   depends_on "libssh2"
   depends_on "openssl@1.1"
   depends_on "pkg-config"
@@ -19,10 +18,6 @@ class Rust < Formula
       s.gsub! "#extended = false", "extended = true"
       s.gsub! '#channel = "dev"', 'channel = "stable"'
       s.gsub! '[target.x86_64-unknown-linux-gnu]', '[target.x86_64-apple-darwin]'
-      s.gsub! '#cc = "cc"', 'cc = "/usr/local/opt/llvm/bin/clang"'
-      s.gsub! '#ar = "ar"', 'ar = "/usr/local/opt/llvm/bin/llvm-ar"'
-      s.gsub! '#ranlib = "ranlib"', 'ranlib = "/usr/local/opt/llvm/bin/llvm-ranlib"'
-      s.gsub! '#llvm-config = "../path/to/llvm/root/bin/llvm-config"', 'llvm-config = "/usr/local/opt/llvm/bin/llvm-config"'
     end
 
     # Fix build failure for compiler_builtins "error: invalid deployment target
@@ -32,8 +27,6 @@ class Rust < Formula
     # Ensure that the `openssl` crate picks up the intended library.
     # https://crates.io/crates/openssl#manual-configuration
     ENV["OPENSSL_DIR"] = Formula["openssl@1.1"].opt_prefix
-    ENV["CC"] = "/usr/local/opt/llvm/bin/clang"
-    ENV["CXX"] = "/usr/local/opt/llvm/bin/clang++"
 
     # Fix build failure for cmake v0.1.24 "error: internal compiler error:
     # src/librustc/ty/subst.rs:127: impossible case reached" on 10.11, and for
