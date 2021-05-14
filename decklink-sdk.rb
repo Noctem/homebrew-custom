ee10de5b38998bae3b8d35f40f401c1732136d5c
David Christenson <mail@noctem.xyz>
2021-05-13 21:28:38 -0600

    DeckLink SDK 12.0

--- decklink-sdk.rb
+++ decklink-sdk.rb
@@ -1,8 +1,8 @@
 class DecklinkSdk < Formula
   desc "BlackMagic Design DeckLink Software Development Kit headers"
   homepage "https://www.blackmagicdesign.com/support/family/capture-and-playback"
-  url "https://github.com/Noctem/DeckLink-SDK/releases/download/v11.4/decklinksdk-11.4.tar.xz"
-  sha256 "015ee778365256a47bfd3913aaeccf1fecaeb7b16f2794b73dea6ed8022a01bf"
+  url "https://github.com/Noctem/DeckLink-SDK/releases/download/v12.0/decklinksdk-12.0.tar.xz"
+  sha256 "4c05ca1a8907fbc7f106654c512028fe21e7754f228971fcce250668a419ca04"
 
   def install
     include.install Dir["include/*"]
